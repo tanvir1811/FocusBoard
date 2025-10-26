3d6fc773c0d3811855f20e46f2e1aa92bfc8d219
tanvir1811 <khantanvirferdoussamrat@gmail.com>
2025-10-26 20:27:43 +0600

    Upload local changes

--- script3.js
+++ script3.js
@@ -3,7 +3,8 @@ function check(){
 
 let parentDiv1=document.getElementById("flex");
 let div2buttonprevious=document.getElementById("controls");
-
+let div1childs=document.getElementsByClassName("f1");
+let div2childs=document.getElementsByClassName("f2");
 let parentDiv2=document.getElementById("flex21");
 
 
@@ -12,16 +13,19 @@ const width=window.innerWidth;
 if(width<=1000){
     parentDiv1.style.display="block";
     parentDiv1.style.marginBottom="70vh";
-    parentDiv1.style.width="100%";
-    parentDiv1.style.justifyContent="center";
-    
+ 
     parentDiv2.style.display="block";
     parentDiv2.style.marginBottom="70vh";
-    parentDiv2.style.width="100%";
-    parentDiv2.style.justifyContent="center";
-    parentDiv2.style.marginLeft="0vw";
-    div2buttonprevious.style.width="80%";
+  
+    div2buttonprevious.style.width="60%";
 
+    for(let i=0;i<div1childs.length;i++){
+        div1childs[i].style.width="100%"
+    }
+
+     for(let i=0;i<div1childs.length;i++){
+        div2childs[i].style.width="100%"
+    }
 
   
 
@@ -41,6 +45,15 @@ else if(width>1000){
     parentDiv2.style.justifyContent="center";
     parentDiv2.style.marginLeft="auto";
     div2buttonprevious.style.width="30vw";
+
+    for(let i=0;i<div1childs.length;i++){
+        div1childs[i].style.width="50%"
+    }
+
+     for(let i=0;i<div1childs.length;i++){
+        div2childs[i].style.width="50%"
+    }
+
 
 
 }
