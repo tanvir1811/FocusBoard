2c5f8c1c03ced1ededd116fa88452961d69bd332
khan Tanvir Ferdous <168400554+tanvir1811@users.noreply.github.com>
2025-10-26 19:14:46 +0600

    Adjust responsive styles for screen width changes

--- script3.js
+++ script3.js
@@ -9,17 +9,17 @@ let parentDiv2=document.getElementById("flex21");
 
 const width=window.innerWidth;
 
-if(width<=1100){
+if(width<=1000){
     parentDiv1.style.display="block";
     parentDiv1.style.marginBottom="70vh";
-    parentDiv1.style.width="180%";
+    parentDiv1.style.width="100%";
     parentDiv1.style.justifyContent="center";
     
     parentDiv2.style.display="block";
     parentDiv2.style.marginBottom="70vh";
-    parentDiv2.style.width="180%";
+    parentDiv2.style.width="100%";
     parentDiv2.style.justifyContent="center";
-    parentDiv2.style.marginLeft="3vw";
+    parentDiv2.style.marginLeft="0vw";
     div2buttonprevious.style.width="80%";
 
 
@@ -29,7 +29,7 @@ if(width<=1100){
 
 }
 
-else if(width>1100){
+else if(width>1000){
     parentDiv1.style.display="flex";
     parentDiv1.style.marginBottom="0vh";
     parentDiv1.style.width="100%";
